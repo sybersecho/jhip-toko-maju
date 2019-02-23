cb6a20478c4312cdd463ebf5c1e9816c56bfdcef
sybersecho <sybers_echo@yahoo.co.id>
2019-02-23 23:50:08 +0700

    remove conflict

--- src/main/webapp/app/entities/customer/customer.route.ts
+++ src/main/webapp/app/entities/customer/customer.route.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Routes } from '@angular/router';
-<<<<<<< HEAD
-=======
 import { JhiPaginationUtil, JhiResolvePagingParams } from 'ng-jhipster';
->>>>>>> a512f2e
 import { UserRouteAccessService } from 'app/core';
 import { Observable, of } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
@@ -36,17 +33,12 @@ export const customerRoute: Routes = [
     {
         path: '',
         component: CustomerComponent,
-<<<<<<< HEAD
-        data: {
-            authorities: ['ROLE_USER'],
-=======
         resolve: {
             pagingParams: JhiResolvePagingParams
         },
         data: {
             authorities: ['ROLE_USER'],
             defaultSort: 'id,asc',
->>>>>>> a512f2e
             pageTitle: 'jhiptokomajuApp.customer.home.title'
         },
         canActivate: [UserRouteAccessService]
