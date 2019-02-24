21db0aef6f6308f272a149b1bebf14d5e27049c0
sybersecho <sybers_echo@yahoo.co.id>
2019-02-24 16:46:13 +0700

    Remove unnecessary code. Close #11

--- src/main/webapp/app/entities/supplier/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './supplier.service';
-export * from './supplier-update.component';
-export * from './supplier-delete-dialog.component';
-export * from './supplier-detail.component';
-export * from './supplier.component';
-export * from './supplier.route';
