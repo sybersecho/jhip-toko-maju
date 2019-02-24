464d7b809f71610c015a578bff279326eed320d1
sybersecho <sybers_echo@yahoo.co.id>
2019-02-24 17:12:48 +0700

    Remove test Code. Closes #11

--- src/main/webapp/app/entities/customer-product/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './customer-product.service';
-export * from './customer-product-update.component';
-export * from './customer-product-delete-dialog.component';
-export * from './customer-product-detail.component';
-export * from './customer-product.component';
-export * from './customer-product.route';
